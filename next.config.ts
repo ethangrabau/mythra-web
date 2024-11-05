c9fbc0077a21e5329075f8ef722082ddd6890ea1
Richard Schmidt <richardnew12511@gmail.com>
2024-11-05 15:22:26 -0600

    add experimental instrumentationHook to next.config.ts. This will allow us to setup the db connection just once on app startup and cache it, instead of having to call dbConnect on every page that needs it.

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,16 @@
 import type { NextConfig } from 'next';
 
 const config: NextConfig = {
-  webpack: (config) => {
+  webpack: config => {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil',
+      bufferutil: 'commonjs bufferutil',
     });
     return config;
   },
+  experimental: {
+    instrumentationHook: true,
+  } as any,
   async rewrites() {
     return [
       {
