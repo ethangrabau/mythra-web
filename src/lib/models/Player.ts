069ab8d9c5f14257c70f97a38a8370473de2b353
Richard Schmidt <richardnew12511@gmail.com>
2024-11-07 16:20:31 -0600

    add player schema

--- src/lib/models/Player.ts
+++ src/lib/models/Player.ts
@@ -4,13 +4,31 @@ import { Schema, model, Document, Types } from 'mongoose';
 interface IPlayer extends Document {
   email: string;
   username: string;
-  displayName: string;
   discord?: string;
-  timezone: string;
-  availability: {
+  timezone?: string;
+  availability?: {
     dayOfWeek: number; // 0-6 for Sunday-Saturday
     startTime: string; // "HH:mm" format
     endTime: string;
   }[];
   activeCampaigns: Types.ObjectId[]; // Reference to campaigns where isActive
 }
+
+// Schema
+const PlayerSchema = new Schema<IPlayer>(
+  {
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    discord: String,
+    timezone: { type: String, required: false },
+    availability: [
+      {
+        dayOfWeek: { type: Number, min: 0, max: 6 },
+        startTime: String,
+        endTime: String,
+      },
+    ],
+    activeCampaigns: [{ type: Schema.Types.ObjectId, ref: 'Campaign' }],
+  },
+  { timestamps: true }
+);
