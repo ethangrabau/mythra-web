97c7648dd2005bb757dd26bca8c0faf3ee410941
Richard Schmidt <richardnew12511@gmail.com>
2024-11-11 13:58:13 -0600

    add CampaignNav component

---
+++ src/app/campaigns/layout.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Plus, List } from 'lucide-react';
+import { cn } from '@/lib/utils/styleUtils';
